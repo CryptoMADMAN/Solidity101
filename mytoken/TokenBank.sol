36c5a9dc6ad87110651b09d4f85209ad4847ba23
pi <1445207429@qq.com>
2024-04-23 10:17:17 +0800

    fix:token bank +erc20

--- mytoken/TokenBank.sol
+++ mytoken/TokenBank.sol
@@ -1,35 +1,38 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
-import "./BaseERC20.sol";  // 假设BaseERC20合约位于同一文件夹
+import "./BaseERC20.sol";
+
+interface IERC20 {
+    function balanceOf(address _owner) external  view returns(uint);
+    function transferFrom(address _from, address _to, uint256 _value) external returns (bool success) ;
+
+    function transfer(address _to, uint256 _value) external returns (bool success);
+}
 
 contract TokenBank {
-    BaseERC20 public token;
-    mapping(address => uint256) public deposits;
+    // BaseERC20 public token;
+    mapping(address => mapping(address=>uint256)) public depositBalances;
 
-    event Deposited(address indexed user, uint256 amount);
-    event Withdrawn(address indexed user, uint256 amount);
-
-    constructor(address _tokenAddress) {
-        token = BaseERC20(_tokenAddress);
+    constructor() {
+        // token = new BaseERC20();
     }
 
-    function deposit(uint256 _amount) public {
-        require(_amount > 0, "Amount must be greater than 0");
-        require(token.balanceOf(msg.sender) >= _amount, "Insufficient token balance");
-        require(token.transferFrom(msg.sender, address(this), _amount), "Transfer failed");
+    function deposit(address _token,uint256 amount) public {
 
-        deposits[msg.sender] += _amount;
-        emit Deposited(msg.sender, _amount);
+        uint256 balanceBefore = IERC20(_token).balanceOf(address(this));
+        bool sent = IERC20(_token).transferFrom(msg.sender, address(this), amount);
+        require(sent, "Token transfer failed");
+        uint256 balanceAfter = IERC20(_token).balanceOf(address(this));
+        uint256 actualAmount = balanceAfter - balanceBefore; // To make sure what is actually received
+        depositBalances[msg.sender][_token] += actualAmount;
     }
 
-    function withdraw(uint256 _amount) public {
-        require(_amount > 0, "Amount must be greater than 0");
-        require(deposits[msg.sender] >= _amount, "Insufficient deposit balance");
+    function withdraw(address _token,uint256 amount) public {
 
-        deposits[msg.sender] -= _amount;
-        require(token.transfer(msg.sender, _amount), "Transfer failed");
-
-        emit Withdrawn(msg.sender, _amount);
+        require(depositBalances[msg.sender][_token] >= amount, "Insufficient balance");
+        depositBalances[msg.sender][_token] -= amount;
+        bool sent = IERC20(_token).transfer(msg.sender, amount);
+        require(sent, "Token transfer failed");
     }
 }
